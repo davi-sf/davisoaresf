170ffdc87c75a1806e129a306a3c4bc84222cab3
davi-sf <davi.soares.ferreira@ccc.ufcg.edu.br>
2024-03-09 01:34:18 -0300

    Primeiro commit da aplicacao

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let menuIcon = document.querySelector('#menu-icon');
+    let navBar = document.querySelector('.navbar')
+    menuIcon.addEventListener('click', () => {
+        menuIcon.classList.toggle('bx-x');
+        navBar.classList.toggle('active');
+    });
+});
